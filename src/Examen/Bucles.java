ec0ac568be6c8796353cac79c851de304349aa5f
MetaCryptDev <alberpinigon@gmail.com>
2023-05-02 10:59:32 +0200

    Clase Bucles hecha

--- src/Examen/Bucles.java
+++ src/Examen/Bucles.java
@@ -1,5 +1,34 @@
 package Examen;
 
+import java.util.Scanner;
+
 public class Bucles {
 
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		System.out.print("Dime el numero limite: ");
+		int numero = sc.nextInt();
+		
+		int contador = 0;
+		while (contador <= numero) {
+			System.out.println(contador);
+			contador++;
+		}
+		
+		System.out.println("");
+		
+		System.out.print("Dime otro numero limite: ");
+		int numero2 = sc.nextInt();
+		
+		int contador2 = 0;
+		while (contador2 <= numero2) {
+			System.out.println(contador2);
+			contador2++;
+			contador2++;
+		}
+		
+	}
+
 }
+
--- src/Examen/TratamientoCadenas.java
+++ src/Examen/TratamientoCadenas.java
@@ -1,5 +1,28 @@
 package Examen;
+import java.util.*;
 
 public class TratamientoCadenas {
-
+	
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		System.out.print("Escribe la primera palabra: ");
+		String palabra1 = sc.next();
+		System.out.print("Escribe la segunda palabra: ");
+		String palabra2 = sc.next();
+		
+		String combinadas= (palabra1 + " " + palabra2);		
+		System.out.println("Tu String es: " + combinadas);
+		System.out.println();
+		
+		System.out.println("Escribeme otra palabra");
+		String palabra3 = sc.next().toLowerCase();
+		int contador = 0;
+		for(int i = 0; i < palabra3.length(); i++) {
+		if(palabra3.substring(i).equals("a")) {
+			contador++;
+			}
+		}
+		System.out.println("Hay " + contador + " As dentro de la palabra " + palabra3);
+	}
 }
